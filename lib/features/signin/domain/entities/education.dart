78a3287118c5c3460ebcb8da369e988078ebca2f
suojae <suojae.developer@gmail.com>
2025-02-24 16:38:16 +0900

    feat: education entity 추가

---
+++ lib/features/signin/domain/entities/education.dart
@@ -0,0 +1,13 @@
+class Education {
+  final int coachId;
+  final String name;
+  final String department;
+  final String degree;
+
+  Education({
+    required this.coachId,
+    required this.name,
+    required this.department,
+    required this.degree,
+  });
+}
