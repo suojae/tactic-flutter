386d3d2d8cfbea53531a47c57408966f7d53917b
suojae <suojae.developer@gmail.com>
2025-02-24 16:38:49 +0900

    feat: work entity 추가

---
+++ lib/features/signin/domain/entities/work.dart
@@ -0,0 +1,13 @@
+class Work {
+  final int coachId;
+  final String name; // 근무처 이름
+  final String rank; // 직급 또는 직책
+  final String period; // 근무 기간 (예: "2018-2022")
+
+  Work({
+    required this.coachId,
+    required this.name,
+    required this.rank,
+    required this.period,
+  });
+}
