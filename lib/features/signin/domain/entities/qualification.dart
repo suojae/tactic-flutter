5928a4f239068b05dfdee7e4863b23ec501bb20b
suojae <suojae.developer@gmail.com>
2025-02-24 16:38:29 +0900

    feat: qualification entity 추가

---
+++ lib/features/signin/domain/entities/qualification.dart
@@ -0,0 +1,13 @@
+class Qualification {
+  final int coachId;
+  final String issuer;
+  final String name;
+  final int fileId;
+
+  Qualification({
+    required this.coachId,
+    required this.issuer,
+    required this.name,
+    required this.fileId,
+  });
+}
